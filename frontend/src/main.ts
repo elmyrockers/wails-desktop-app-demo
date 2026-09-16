eb4b5315519334544c8fe19fb567c36335df5e30
elmyrockers <elmyrockers@gmail.com>
2026-09-17 00:00:30 +0800

    Update main ts file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Import individual generated functions from the service module
+import { Greet, GreetMany } from "../bindings/github.com/elmyrockers/wails-desktop-app-demo/greetservice";
 
-createApp(App).mount('#app')
+
+
+createApp(App).mount('#app');
